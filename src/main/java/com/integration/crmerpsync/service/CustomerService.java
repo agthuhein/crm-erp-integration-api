dd826c66949be9bd24374575c7d16c7ae96f66b7
Aung Thu Hein <agthuhein.mm@gmail.com>
2025-12-15 11:06:58 +0100

    customer service class update

--- src/main/java/com/integration/crmerpsync/service/CustomerService.java
+++ src/main/java/com/integration/crmerpsync/service/CustomerService.java
@@ -1,6 +1,7 @@
 package com.integration.crmerpsync.service;
 
 import com.integration.crmerpsync.DTO.CustomerCreate;
+import com.integration.crmerpsync.DTO.CustomerUpdate;
 import com.integration.crmerpsync.Repository.CustomerRepository;
 import com.integration.crmerpsync.entity.Customer;
 import lombok.RequiredArgsConstructor;
@@ -9,22 +10,47 @@ import org.springframework.data.domain.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.Instant;
+
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
 
-    public Customer create(CustomerCreate request){
-        if(customerRepository.existsByEmail(request.getEmail())){
+    public Customer create(CustomerCreate create){
+        if(customerRepository.existsByEmail(create.getEmail())){
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Email already in use");
         }
 
         Customer customer = Customer.builder()
-                .name(request.getName())
-                .email(request.getEmail())
-                .phone(request.getPhone())
+                .name(create.getName())
+                .email(create.getEmail())
+                .phone(create.getPhone())
                 .build();
 
         return customerRepository.save(customer);
+    }
+
+    public Customer update(Long id, CustomerUpdate update){
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found."));
+
+        customer.setName(update.getName());
+        customer.setPhone(update.getPhone());
+        return customerRepository.save(customer);
+    }
+
+    public Page<Customer> customers(Integer page, Integer size, Instant updatedAt){
+        Pageable pg = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "updatedAt" ));
+
+        if(updatedAt !=null){
+            return customerRepository.findByUpdatedAtAfter(updatedAt, pg);
+        }
+        return customerRepository.findAll(pg);
+    }
+
+    public Customer getById(Long id){
+        return customerRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found."));
     }
 }
