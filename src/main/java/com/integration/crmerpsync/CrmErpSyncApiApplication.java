f369c3c63ac06de27c5a253ec831b383f9469dea
Aung Thu Hein <agthuhein.mm@gmail.com>
2025-12-15 10:16:54 +0100

    Initial commit - Spring Boot CRM ERP Sync

---
+++ src/main/java/com/integration/crmerpsync/CrmErpSyncApiApplication.java
@@ -0,0 +1,13 @@
+package com.integration.crmerpsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmErpSyncApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmErpSyncApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/integration/crmerpsync/CrmErpSyncApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integration.crmerpsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmErpSyncApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
